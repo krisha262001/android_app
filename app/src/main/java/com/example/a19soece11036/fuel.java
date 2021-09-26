f8bcf79edab13400205b0daf5229da884d478ce8
Krisha <krishasureja26@gmail.com>
2021-09-26 09:57:41 +0530

    Android

--- app/src/main/java/com/example/a19soece11036/fuel.java
+++ app/src/main/java/com/example/a19soece11036/fuel.java
@@ -30,15 +30,15 @@ public class fuel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuel);
 
-            Toast.makeText(getApplicationContext(),"FuelRefill",Toast.LENGTH_LONG).show();
-            e1 = (EditText) findViewById(R.id.etdate);
-            e2 = (EditText) findViewById(R.id.ettime);
-            e3 = (EditText) findViewById(R.id.etodo);
-            e4 = (EditText) findViewById(R.id.etlodo);
-            e5 = (EditText) findViewById(R.id.etprice);
-            e6 = (EditText) findViewById(R.id.etfu);
-            e7 = (EditText) findViewById(R.id.etcost);
-            e8 = (EditText) findViewById(R.id.etavg);
+        Toast.makeText(getApplicationContext(), "FuelRefill", Toast.LENGTH_LONG).show();
+        e1 = (EditText) findViewById(R.id.etdate);
+        e2 = (EditText) findViewById(R.id.ettime);
+        e3 = (EditText) findViewById(R.id.etodo);
+        e4 = (EditText) findViewById(R.id.etlodo);
+        e5 = (EditText) findViewById(R.id.etprice);
+        e6 = (EditText) findViewById(R.id.etfu);
+        e7 = (EditText) findViewById(R.id.etcost);
+        e8 = (EditText) findViewById(R.id.etavg);
         Button submit = (Button) findViewById(R.id.submit);
         databaseHelper = new DatabaseHelper(this);
 
@@ -55,41 +55,38 @@ public class fuel extends AppCompatActivity {
             }
         });
 
-        }
         submit.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                Intent FuelIntent = new Intent(fuel.this,FuelRecord.class);
+                Intent FuelIntent = new Intent(fuel.this, FuelRecord.class);
                 startActivity(FuelIntent);
             }
         });
-        }
+    }
 
-    public void AddData(int newEntry){
+    public void AddData(int newEntry) {
         boolean insertData = databaseHelper.addData(newEntry);
-        if(insertData){
+        if (insertData) {
             toastMessage("Data Successfully Inserted");
-        }else{
+        } else {
             toastMessage("Enter valid detail");
         }
+    }
 
-    public void Average(View Object view;
-        view)
+        public void Average (View view)
         {
             double a;
-            if(e5.length()>0 && e6.length()>0 && e7.length()>0)
-            {
-                double km=Double.parseDouble(e5.getText().toString());
-                double sr=Double.parseDouble(e6.getText().toString());
-                double pr=Double.parseDouble(e7.getText().toString());
-                a=(sr*(km/100)*pr)/10;
-                a=Math.round(a*100.00)/100.00;
+            if (e5.length() > 0 && e6.length() > 0 && e7.length() > 0) {
+                double km = Double.parseDouble(e5.getText().toString());
+                double sr = Double.parseDouble(e6.getText().toString());
+                double pr = Double.parseDouble(e7.getText().toString());
+                a = (sr * (km / 100) * pr) / 10;
+                a = Math.round(a * 100.00) / 100.00;
                 e8.setText((int) a);
-            }
-            else Toast.makeText(getApplicationContext(),"Enter valid detail",Toast.LENGTH_LONG).show();
+            } else
+                Toast.makeText(getApplicationContext(), "Enter valid detail", Toast.LENGTH_LONG).show();
         }
-    }
 
     private void toastMessage(String message) {
-        Toast.makeText(this, message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+}
